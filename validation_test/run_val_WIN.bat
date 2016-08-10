ac483db01e9eed162bf40baa8862e3a3e8db6537
Luyao Zou <zouluyao@hotmail.com>
2016-08-10 14:01:56 -0400

    Update validation WIN batch script

--- validation_test/run_val_WIN.bat
+++ validation_test/run_val_WIN.bat
@@ -1,16 +1,26 @@
 @echo off
 echo "Windows validation script"
+
 rem delete old files
 del val*.csv
-rem generate single sweep validation outputs
-python ..\sweep.py sample_input_single_int.dat -bdwth 18 -bg 5 -lo sample_input_LO.dat -o val_single_bg5_mode1.csv
-python ..\sweep.py sample_input_single_int.dat -bdwth 18 -bg 5 -lo sample_input_LO.dat -nobase -o val_single_bg5_mode1_nobase.csv
-python ..\sweep.py sample_input_single_int.dat -bdwth 18 -bg 5 -lo sample_input_LO.dat -spline -o val_single_bg5_mode1_spline.csv
-rem python ..\sweep.py sample_input_single_int.dat -bdwth 18 -bg 5 -lo sample_input_LO.dat -mode 2 -o val_single_bg5_mode2.csv
-rem generate fullband sweep validation outputs
-python ..\sweep.py sample_input_fullband_int.dat -bg 5 -cf sample_input_fullband_freq.dat -lo sample_input_LO.dat -bg 5 -o val_fb_bg5_mode1.csv
-python ..\sweep.py sample_input_fullband_int.dat -bg 5 -cf sample_input_fullband_freq.dat -lo sample_input_LO.dat -bg 5 -nobase -o val_fb_bg5_mode1_nobase.csv
-python ..\sweep.py sample_input_fullband_int.dat -bg 5 -cf sample_input_fullband_freq.dat -lo sample_input_LO.dat -bg 5 -spline -o val_fb_bg5_mode1_spline.csv
+
+rem $generate single sweep validation outputs
+rem fg and bg options
+python ../sweep.py sample_input_single_inten.dat -bdwth 18 -bg 5 -lo sample_input_fb_couple-of-lines_lo.dat -box 5 -o val_single_fg1-bg5_box5.csv
+python ../sweep.py sample_input_single_inten.dat -bdwth 18 -fg 2 -bg 5 -lo sample_input_fb_couple-of-lines_lo.dat -o val_single_fg2-bg5.csv
+python ../sweep.py sample_input_single_inten.dat -bdwth 18 -fg 5 -bg 1 -lo sample_input_fb_couple-of-lines_lo.dat -box 5 -o val_single_fg5-bg1_box5.csv
+python ../sweep.py sample_input_single_inten.dat -bdwth 18 -lo sample_input_fb_couple-of-lines_lo.dat -box 5 -o val_single_avg.csv
+rem spline & nobase options
+python ../sweep.py sample_input_single_inten.dat -bdwth 18 -bg 5 -lo sample_input_fb_couple-of-lines_lo.dat -nobase -o val_single_fg1-bg5_nobase.csv
+python ../sweep.py sample_input_single_inten.dat -bdwth 18 -bg 5 -lo sample_input_fb_couple-of-lines_lo.dat -spline -o val_single_fg1-bg5_spline.csv
+
+rem $generate fullband sweep validation outputs
+for /l %%f in (couple-of-lines numerous-lines wavybase-noline) do (
+  python ../sweep.py "sample_input_fb_%%f""_inten."* -cf "sample_input_fb_%%f""_freq."* -bg 5 -lo "sample_input_fb_%%f""_lo."* -box 5 -o "val_fb_%%f""_box5.csv"
+  python ../sweep.py "sample_input_fb_%%f""_inten."* -cf "sample_input_fb_%%f""_freq."* -bg 5 -lo "sample_input_fb_%%f""_lo."* -nobase -o "val_fb_%%f""_nobase.csv"
+  python ../sweep.py "sample_input_fb_%%f""_inten."* -cf "sample_input_fb_%%f""_freq."* -bg 5 -lo "sample_input_fb_%%f""_lo."* -spline -o "val_fb_%%f""_spline.csv"
+)
+
 rem run python validation script
 python validation.py
 pause
